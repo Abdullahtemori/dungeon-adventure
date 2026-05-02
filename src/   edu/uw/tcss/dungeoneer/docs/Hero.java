020dc1e1421cbd28b125647ea4e0b0151d86e39d
DaniellaHB <dhbirung@uw.edu>
2026-05-02 07:38:03 -0700

    Enhance Hero class with inventory management
    Added fields for healing, vision potions, bombs, and collected pillars. Updated methods to manage inventory and return hero info.

--- src/   edu/uw/tcss/dungeoneer/docs/Hero.java
+++ src/   edu/uw/tcss/dungeoneer/docs/Hero.java
@@ -1,5 +1,7 @@
 package edu.uw.tcss.dungeoneer.model;
 
+import java.util.Hashset;
+import java.util.Set
 /**
  * Abstract class representing a Hero in the dungeon.
  * Heroes can block attacks and have a unique special skill.
@@ -11,6 +13,18 @@ public abstract class Hero extends DungeonCharacter {
 
     /** Probability that this hero blocks an incoming attack (0.0–1.0). */
     private double myChanceToBlock;
+
+     /** Number of healing potions in the hero's inventory. */
+    private int myHealingPotions;
+
+    /** Number of vision potions in the hero's inventory. */
+    private int myVisionPotions;
+
+    /** Number of bombs in the hero's inventory. */
+    private int myBombs;
+
+    /** Set of pillars the hero has collected. */
+    private Set<Pillar> myPillarsFound;
 
     /**
      * Constructs a Hero with all required stats.
@@ -29,6 +43,11 @@ public abstract class Hero extends DungeonCharacter {
                    final double theChanceToBlock) {
         super(theName, theHP, theMinDmg, theMaxDmg, theSpeed, theChanceToHit);
         myChanceToBlock = theChanceToBlock;
+
+        myHealingPotions = 0;
+        myVisionPotions = 0;
+        myBombs = 0;
+        myPillarsFound = new Hashset<>();
     }
 
     /**
@@ -51,12 +70,131 @@ public abstract class Hero extends DungeonCharacter {
     public double getChanceToBlock() { return myChanceToBlock; }
 
     /**
+     * Returns the number of healing potions in the hero's inventory.
+     *
+     *@return healing potion count
+     */
+    public int getHealingPotions() {
+        return myHealingPotions;
+    }
+
+     /**
+     * Returns the number of vision potions in the hero's inventory.
+     *
+     *@return vision potion count
+     */
+    public int getVisionPotions() {
+        return myVisionPotions;
+    }
+
+     /**
+     * Returns the number of bombs in the hero's inventory.
+     *
+     *@return bomb count
+     */
+    public int getBombs() {
+        return myBombs;
+    }
+    
+    /**
+     * Returns the set of pillars the hero has collected.
+     *
+     *@return set of collected pillars
+     */
+    public Set<Pillar> getPillarsFound() {
+        return myPillarsFound;
+    }
+
+    /**
+     * Adds one healing potion to the hero's inventory.
+     */
+    public void addHealingPotion() {
+        myHealingPotions++;
+    }
+
+    /**
+     * Adds one vision potion to the hero's inventory.
+     */
+    public void addVisionPotion() {
+        myVisionPotions++;
+    }
+
+    /**
+     * Adds one Bomb to the hero's inventory.
+     */
+    public void addBomb() {
+        myBombs++;
+    }
+
+    /**
+     * Adds a pillar to the hero's collected pillars set.
+     * Duplicate pillars are ignored automatically by the set.
+     *
+     * @param thePillar the pillar to add
+     */
+    public void addPillar(final Pillar thePillar) {
+        myPillarsFound.add(thePillar);
+    }
+
+    /**
+     * Uses one healing potion if available.
+     * Returns the amount healed, or 0 if none is available.
+     *
+     * @return HP healed, or 0 if no potions available
+     */
+    public int usehealingPotion(){
+        if (myhealingPotions <= 0){
+            return 0;
+        }
+        myHealingPotions--;
+        final HealingPotion potion = new HealingPotion();
+        return potion.getHealAmount();
+    }
+
+    /**
+     * Uses one vision potion if available.
+     * Returns true if successful, false if none is available.
+     *
+     * @return true if potion was used, false if none left
+     */
+    public int useVisionPotion(){
+        if (myVisionPotions <= 0){
+            return false;
+        }
+        myVisionPotions--;
+        return true;
+    }
+
+    /**
+     * Uses one bomb against a target monster if available.
+     * Returns the damage dealt, or 0 if none is available.
+     *
+     * @param theTarget the monster to bomb
+     * @return damage dealt, or 0 if no bombs are left
+     */
+    public int useBomb(final Monster theTarget){
+        if (myBombs <= 0){
+            return 0;
+        }
+        myBombs--;
+        final Bomb bomb = new Bomb();
+        final int  damage = bomb.getDamage();
+        theTarget.takeDamage(damage);
+        return damage;
+    }
+    /**
      * Returns hero info as a string.
      *
      * @return formatted hero info
      */
     @Override
     public String toString() {
-        return super.toString() + " | Block Chance: " + myChanceToBlock;
+        return super.toString() 
+            + " | Block Chance: " + myChanceToBlock
+            + " | Healing Potions: " + myHealingPotions
+            + " | Vision Potions: " + myVisionPotions
+            + " | Bombs: " + myBombs
+            + " | Pillars Found: " + myPillarsFound;
+        
     }
 }
