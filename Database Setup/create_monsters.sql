f09d41adbafaf11244f05473c6489d470e14da69
Abdullahtemori <114013439+Abdullahtemori@users.noreply.github.com>
2026-05-29 14:57:37 -0700

    Delete Database Setup/create_monsters.sql

--- Database Setup/create_monsters.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE monsters (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-);
-
-INSERT INTO monsters (name) VALUES ('Ogre');
-INSERT INTO monsters (name) VALUES ('Gremlin');
-INSERT INTO monsters (name) VALUES ('Skeleton');
