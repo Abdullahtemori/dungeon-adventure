644ac959d4149493badf6bb817b67ecb1d93b4f7
Abdullahtemori <114013439+Abdullahtemori@users.noreply.github.com>
2026-05-29 14:54:58 -0700

    Create create_monsters.sql

---
+++ Database Setup/create_monsters.sql
@@ -0,0 +1,8 @@
+CREATE TABLE monsters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+INSERT INTO monsters (name) VALUES ('Ogre');
+INSERT INTO monsters (name) VALUES ('Gremlin');
+INSERT INTO monsters (name) VALUES ('Skeleton');
